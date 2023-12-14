a9d739b97b6ec6e00100db10b97ce65d6337c1e5
Lahusa <lasse@huber-saffer.de>
2023-12-14 19:55:56 +0100

    Implemented TCP server groundwork

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Server
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Starting Cettlers of Satan Server");
+
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[1];
+            const int port = 17017;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            Console.WriteLine($"Server listening on {ipAddress}:{port}");
+
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(localEndPoint);
+            socket.Listen(5);
+            Socket handler = socket.Accept();
         }
     }
 }
