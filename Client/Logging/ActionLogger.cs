07fba130351a746ed9f8b4cc71e1d995777135f1
Lahusa <lasse@huber-saffer.de>
2025-02-26 18:34:32 +0100

    Improved log readability

--- Client/Logging/ActionLogger.cs
+++ Client/Logging/ActionLogger.cs
@@ -1,4 +1,6 @@
 ﻿using Common;
+using Common.Actions;
+using SFML.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,11 +30,42 @@ namespace Client.Logging
 
         public void Log(Action action, GameState state)
         {
+            Color playerColor = ColorPalette.GetPlayerColor(action.PlayerIndex);
+
             // Action entry
-            switch(action)
+            switch (action)
             {
+                case EndTurnAction endTurnAction:
+                    _log.WriteLine(new ColoredStrEntry("Ended turn", playerColor));
+                    break;
+
+                case RollAction rollAction:
+                    _log.WriteLine(new ColoredStrEntry($"Rolled {rollAction.RollResult.Total} ({rollAction.RollResult.First}+{rollAction.RollResult.Second})", playerColor));
+                    // TODO: Yield summary
+                    break;
+
+                case DiscardAction discardAction:
+                    StringBuilder sb = new StringBuilder();
+                    foreach (ResourceCardType resourceType in CardSet<ResourceCardType>.Values)
+                    {
+                        for (int i = 0; i < discardAction.SelectedCards.Get(resourceType); i++)
+                        {
+                            sb.Append(resourceType.GetAbbreviation());
+                        }
+                    }
+                    _log.WriteLine(new ColoredStrEntry($"Discarded {discardAction.SelectedCards.Count()} ({sb})", playerColor));
+                    break;
+
+                case RobberAction robberAction:
+                    _log.WriteLine(new ColoredStrEntry("Moved the robber", playerColor));
+                    // TODO: Stolen card and victim
+                    break;
+
+                // TODO: Remaining Actions in order used by LegalActionProvider
+
+                // Fallback: Basic string conversion of action in player color
                 default:
-                    _log.WriteLine(new ColoredStrEntry(action.ToString(), ColorPalette.GetPlayerColor(action.PlayerIndex)));
+                    _log.WriteLine(new ColoredStrEntry(action.ToString(), playerColor));
                     break;
             }
 
